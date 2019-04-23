9226e8a7656d3bda7c663abe9c4d352955eb6851
Mike Rossetti <s2t2@users.noreply.github.com>
2019-04-23 18:16:01 -0400

    Verify ability to run the script, seeing lots of requests

---
+++ README.md
@@ -0,0 +1,21 @@
+# Student Support (MovieDB API)
+
+## Setup
+
+Sign up for account, then an API Key: https://www.themoviedb.org/settings/api
+
+Setup ".env" file with env var called `API_KEY`.
+
+From within an anaconda virtual environment, install package dependencies:
+
+```sh
+pip install requests python-dotenv
+```
+
+## Usage
+
+From within an anaconda virtual environment, run the script:
+
+```sh
+python my_script.py
+```
--- my_script.py
+++ my_script.py
@@ -1,3 +1,17 @@
+import os
+import json
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.environ.get("API_KEY")
+
+# inferring these from https://developers.themoviedb.org/3/genres/get-movie-list
+genre_list = ["action", "adventure", "documentary", "horror", "mystery", "science fiction"]
+print("GENRES:", genre_list)
+
 # supplied by MC on 4/22...
 
 
@@ -14,25 +28,27 @@ while True:
     genre_input = input("Enter the name of the genre you want: ")
     if genre_input.lower() in genre_list:
 
-      for collection in range(number_of_movies):
-        data = requests.get("https://api.themoviedb.org/3/movie/" + str(collection) + "?api_key=______________")
+        for collection in range(number_of_movies):
+            request_url = "https://api.themoviedb.org/3/movie/" + str(collection) + f"?api_key={API_KEY}"
+            print(request_url)
+            data = requests.get(request_url)
 
-        if data.status_code == 200:
-          json_string = data.text
-          obj = json.loads(json_string)
+            if data.status_code == 200:
+                json_string = data.text
+                obj = json.loads(json_string)
 
-          for ele in obj['genres']:
-            if genre_input == ele['name'].lower():
-              print(obj['original_title'])
+                for ele in obj['genres']:
+                    if genre_input == ele['name'].lower():
+                        print(obj['original_title'])
 
             # GOT CODE FROM STACK OVERFLOW
     else:
-      print("Please make sure to enter a valid genre")
+        print("Please make sure to enter a valid genre")
 
-      if "Error" in data.text:
-        print("The genre you are looking for is not here")
-      else:
-        break
+        if "Error" in data.text:
+            print("The genre you are looking for is not here")
+        else:
+            break
 
     # GOT SOME OF THIS CODE FROM THE ROBO ADVISOR PROJECT
 
